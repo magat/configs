30fbce8ea888bcced710741515f1e8000859dfba
Mathieu Agar <mathieu.agar@Mathieus-MacBook-Pro.local>
2019-12-13 11:47:56 +0100

    Small improvements: gitignore and dev tools

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -3,6 +3,7 @@
 SETUP=$(pwd)
 
 ln -s ${SETUP}/git/config ~/.gitconfig
+ln -s ${SETUP}/git/ignore ~/.gitignore
 ln -s ${SETUP}/bash/bashrc ~/.bash_profile
 ln -s ${SETUP}/psqlrc ~/.psqlrc
 ln -s ${SETUP}/vimrc ~/.vimrc
